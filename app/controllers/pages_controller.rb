12a07d19f79ed938ed5801e63f10e7fd5fc72a12
Vincent <vincenttouchthis@gmail.com>
2015-03-24 14:47:26 +0800

    Added Dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, only: [:dashboard]
+  
   def home
   	if current_user
-  redirect_to books_path
+  		redirect_to books_path
+  	end
   end
+ 
+  def dashboard
+  	@books = current_user.books
+  end
+  
 end
-end
-
