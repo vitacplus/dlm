e68036558eabe28a3a4aad0fc3b1c2d52176b92d
Vincent <vincenttouchthis@gmail.com>
2015-03-25 09:37:24 +0800

    added image to

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324162616) do
+ActiveRecord::Schema.define(version: 20150325013529) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20150324162616) do
     t.string   "resource_content_type"
     t.integer  "resource_file_size"
     t.datetime "resource_updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
